f714888fbe5138a44c13208e56cf7335f408f7ca
ihValery <ihValery@gmail.com>
2021-10-26 19:48:19 +0300

    Initial Commit

---
+++ CircleMenu/CircleMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircleMenuApp.swift
+//  CircleMenu
+//
+//  Created by Валерий Игнатьев on 26.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct CircleMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CircleMenu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CircleMenu
+//
+//  Created by Валерий Игнатьев on 26.10.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
